1b4a10dc4a2ca9cc606e6c32219ab6473aec30a4
Lukasz Szymczyk <houseplayer@gmail.com>
2019-11-17 20:22:00 +0100

    Firebase Firestore and Authentication added. Creating new users, login, logout options working. Articles visible for users only

--- public/main.js
+++ public/main.js
@@ -1,56 +1,109 @@
-const form = document.querySelector("form");
+//assigning DOM elements to variables
+const messageForm = document.querySelector("#message-form");
+const signupForm = document.querySelector("#signup-form");
+const loginForm = document.querySelector("#login-form");
+const loginButton = document.querySelectorAll(".login");
+const logoutButton = document.querySelectorAll(".logout");
+const signupButton = document.querySelectorAll(".signup");
+const userNameBig = document.querySelector(".user-name-bigscreen");
+const userNameSmall = document.querySelector(".user-name-smallscreen");
+const articlesList = document.querySelector("#articles-list");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  form.reset()
+//user state change listener
+auth.onAuthStateChanged(user => {
+  if (user) {
+    //get articles from firestore and display in DOM
+    db.collection("Articles").get()
+      .then(docs => {
+        docs.forEach(doc => {
+          articlesList.style.display = "block";
+          articlesList.innerHTML += `
+        <li>
+        <div class="collapsible-header">${doc.data().title}</div>
+        <div class="collapsible-body">${doc.data().content}
+          </p>
+          <div class="divider"></div>
+          <p> <a href="#header" class="teal-text link">GO TO TOP</a></p>
+        </div>
+      </li>
+        `
+        })
+      });
+    articlesList.nextElementSibling.style.display = "none";
+    console.log(user.email, "is logged in");
+    loginButton.forEach(item => item.style.display = "none");
+    signupButton.forEach(item => item.style.display = "none");
+    logoutButton.forEach(item => item.style.display = "block");
+    userNameBig.innerHTML = `${user.email}`;
+    userNameBig.style.display = "block";
+    userNameSmall.innerHTML = `${user.email}`;
+    userNameSmall.style.display = "block";
+    document.querySelector(".nav-list").style.display = "block";
+  }
+  else {
+    articlesList.innerHTML = null;
+    articlesList.style.display = "none";
+    articlesList.nextElementSibling.style.display = "block";
+    console.log("no user is currently logged in");
+    loginButton.forEach(item => item.style.display = "block");
+    signupButton.forEach(item => item.style.display = "block");
+    logoutButton.forEach(item => item.style.display = "none");
+    userNameBig.innerHTML = null;
+    userNameBig.style.display = "none";
+    userNameSmall.innerHTML = null;
+    userNameSmall.style.display = "none";
+    document.querySelector(".nav-list").style.display = "block";
+  }
 })
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   var elems = document.querySelectorAll('.parallax');
-//   var instances = M.Parallax.init(elems);
-// });
+//Create new user
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
+  auth.createUserWithEmailAndPassword(email, password)
+    .then((credential) => {
+      console.log(credential.user.email, "has created an account")
+      signupForm.reset();
+      const signupModal = document.querySelector("#signup-modal");
+      M.Modal.getInstance(signupModal).close()
+    })
+    .catch((error) => document.querySelector("#signup-error").innerHTML = error.message.toUpperCase());
+})
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   var elems = document.querySelectorAll('.sidenav');
-//   var instances = M.Sidenav.init(elems);
-// });
+//login
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      loginForm.reset();
+      const loginModal = document.querySelector("#login-modal");
+      M.Modal.getInstance(loginModal).close();
+    })
+    .catch((error) => document.querySelector("#login-error").innerHTML = error.message.toUpperCase());
+})
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   var elems = document.querySelectorAll('.materialboxed');
-//   var instances = M.Materialbox.init(elems);
-// });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   var elems = document.querySelectorAll('.scrollspy');
-//   var instances = M.scrollSpy.init(elems);
-// });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   var elems = document.querySelectorAll('.textarea');
-//   var instances = M.textarea.init(elems);
-// });
-
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems, {
-    accordion: false
+//logout
+document.querySelectorAll(".logout").forEach(item => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    auth.signOut()
+      .then(() => console.log("user logged out"))
   })
-});
+})
 
+//sending message
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  messageForm.reset()
+})
+
+//Initializing materialize
 M.AutoInit();
 
-document.querySelector("#link1").addEventListener("click", () => {
-  document.querySelector("#article1").style.display = "none";
-  document.querySelector("#article2").style.display = "none";
-  document.querySelector("#article3").style.display = "none"
-});
-document.querySelector("#link2").addEventListener("click", () => {
-  document.querySelector("#article1").style.display = "none";
-  document.querySelector("#article2").style.display = "none";
-  document.querySelector("#article3").style.display = "none"
-});
-document.querySelector("#link3").addEventListener("click", () => {
-  document.querySelector("#article1").style.display = "none";
-  document.querySelector("#article2").style.display = "none";
-  document.querySelector("#article3").style.display = "none"
-});
+//closing articles when scrolling to page top
+articlesList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link")) { e.target.parentElement.parentElement.style.display = "none" }
+})
